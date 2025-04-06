2ea8c96c2d00a3021ee7e4c10b0653a89f5ebbfb
lolikgiovi <lolik.giovi@gmail.com>
2025-04-07 06:29:52 +0700

    adjust huey config

--- apps/transactions/tasks.py
+++ apps/transactions/tasks.py
@@ -1,6 +1,13 @@
-from huey.contrib.djhuey import task
+from huey.contrib.djhuey import task, periodic_task
+from huey import crontab
 from .methods import send_notifications
 
+# scheduler
+@periodic_task(crontab(hour=0, minute=5))
+def daily_notification_check():
+    send_notifications()
+
+# manual
 @task()
 def process_notifications():
     send_notifications()
