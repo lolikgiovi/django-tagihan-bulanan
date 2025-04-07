5df732e13880cf90fc619c1665b4e02c9be55315
lolikgiovi <lolik.giovi@gmail.com>
2025-04-07 08:15:50 +0700

    fix config

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1vkjh%qu4^m*_a3rpfwsebip_u_nx&btv!-$*m!&$u&9$sf^9f"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG_SETTING", "False"),
 
 ALLOWED_HOSTS = []
 
